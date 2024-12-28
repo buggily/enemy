cceb4e3443ca5e116f7bd9d6350cc1216e4c5ce8
Lance Holsten <58127678+buggily@users.noreply.github.com>
2024-12-27 20:02:21 -0600

    clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,4 @@ plugins {
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.serialization) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
 }
